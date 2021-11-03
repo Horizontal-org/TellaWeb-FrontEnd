7c2c8740ba808343363348d137b55b50f58895ac
Marcos Gutierrez <gmarcos87@gmail.com>
2021-11-03 09:21:50 +0100

    add api proxy to solve cookies problems

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+        destination: process.env.NEXT_REDIRECT_API_URL + "/:path*",
+      },
+    ];
+  },
 };
