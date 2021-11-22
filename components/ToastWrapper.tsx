885b49449c9576684675201fea12853fdfb11006
juandans01@gmail.com <juandans01@gmail.com>
2021-11-22 14:25:48 -0300

    progress on toast

--- components/ToastWrapper.tsx
+++ components/ToastWrapper.tsx
@@ -1,34 +1,55 @@
 import { FunctionComponent, Fragment, useState } from 'react'
 import { createContext } from '../common/Context'
 import cn from 'classnames'
+import { BsX } from "@react-icons/all-files/bs/BsX"
+import { AiOutlineInfoCircle } from "@react-icons/all-files/ai/AiOutlineInfoCircle"
+import { IoMdCheckmarkCircleOutline } from "@react-icons/all-files/io/IoMdCheckmarkCircleOutline"
 
 type Props = {
   children: React.ReactNode
 }
 
-export const [toastContext, useToast] = createContext<(text: string) => void>()
+export const [toastContext, useToast] = createContext<(text: string, type?: string) => void>()
 
 export const ToastWrapper: FunctionComponent<Props> = ({ children }) => {
 
   const [showToast, toggleToast] = useState<boolean>(false)
   const [isFading, toggleFading] = useState<boolean>(false)
   const [toastText, handleToastText] = useState<string>('')
+  const [toastType, handleType] = useState<string>('')
 
-  const handleToast = (newToastText: string) => {
+  const handleToast = (newToastText: string, type?: string) => {
     handleToastText(newToastText)
-    console.log('do the toast')
+    handleType(type || 'info')
     toggleFading(false)
     toggleToast(true)
 
-    const fadeToken = setTimeout(() => {
+    setTimeout(() => {
       toggleFading(true)
     }, 4000)
 
-    const timeoutToken = setTimeout(() => {
+    setTimeout(() => {
       toggleToast(false)
     }, 5000)
   }
  
+  const clearToast = () => {
+    toggleFading(true)
+    setTimeout(() => {
+      toggleToast(false)
+    }, 1000)
+  }
+
+  const getToastIcon = () => {
+    if (toastType === 'info' || toastType === 'danger') {      
+      return (<AiOutlineInfoCircle color='white' size={20}/>)
+    } else if (toastType === 'success') {
+      return (<IoMdCheckmarkCircleOutline color='white' size={20}/>)
+    }
+  }
+
+  console.log(toastType)
+
   return (
     <toastContext.Provider value={handleToast}>
       <Fragment>
@@ -36,18 +57,30 @@ export const ToastWrapper: FunctionComponent<Props> = ({ children }) => {
         { showToast && (
           <div 
             className={cn(
-              'fixed shadow-md p-4 transition duration-500 bg-blue-600 text-white rounded w-48',
+              'fixed shadow-md p-2 transition text-sm font-bold font-sans duration-300 text-white rounded',
               {
                 'opacity-100': !isFading,
-                'opacity-0': isFading
+                'opacity-0': isFading,
+                'bg-blue-300': toastType === 'info',
+                'bg-red-600': toastType === 'danger',
+                'bg-green-600': toastType === 'success',
               }
             )}
             style={{
               bottom: 15,
-              right: 30             
+              right: 30,
+              minWidth: '20rem'
             }}
           >
-            { toastText }
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
+                { getToastIcon() }
+                <span className='pl-2'>{ toastText }</span>
+              </div>
+              <div className='cursor-pointer' onClick={clearToast}>
+                <BsX color='white' size={20} /> 
+              </div>
+            </div>
           </div>
         )}
       </Fragment>
