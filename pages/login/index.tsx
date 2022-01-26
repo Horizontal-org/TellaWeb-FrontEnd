f1a647ebb57665b56a8232b506248d43eceaf675
Marcos Gutierrez <gmarcos87@gmail.com>
2022-01-26 09:13:15 +0100

    set user after login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -11,24 +11,35 @@ import {
 } from "packages/state/features/auth/authSlice";
 import { useUserProfile } from "packages/state/features/user/userHooks";
 import { Credential } from "packages/state/domain/user";
+import { setUser } from "packages/state/features/user/userSlice";
+import { useLazyGetProfileQuery } from "packages/state/services/user";
 
 const Login = () => {
-  const { errorMessage } = useAuth();
+  const { errorMessage, accessToken } = useAuth();
   const user = useUserProfile();
   const router = useRouter();
   const dispatch = useDispatch();
   const [login, { isLoading }] = useLoginMutation();
+  const [loadUserProfile, { data }] = useLazyGetProfileQuery();
 
   useEffect(() => {
     if (user) router.replace("/report");
   }, [user]);
+
+  useEffect(() => {
+    if (accessToken && !user && !data) {
+      loadUserProfile();
+      return;
+    }
+    if (data) dispatch(setUser(data));
+  }, [accessToken, user, data]);
 
   const doLogin = async (credential: Credential) => {
     try {
       const data = await login(credential).unwrap();
       dispatch(setCredentials(data));
     } catch (err) {
-      dispatch(setError(err.data.message));
+      dispatch(setError(err.data.message || "Something went wrong, try again"));
     }
   };
 
