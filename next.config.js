b6ea2db1b0ae491ef15d3a0d83c1e23be36b24d8
Marcos Gutierrez <gmarcos87@gmail.com>
2022-02-06 13:08:07 +0100

    try locale: false

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   reactStrictMode: true,
-  rewrites: [
-    {
-      source: process.env.NEXT_PUBLIC_API_URL + "/:path*",
-      destination: process.env.NEXT_REDIRECT_API_URL + "/:path*",
-      basePath: false,
-    },
-  ],
+  async rewrites() {
+    return [
+      {
+        source: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+        destination: process.env.NEXT_REDIRECT_API_URL + "/:path*",
+        basePath: false,
+        locale: false,
+      },
+    ];
+  },
   i18n,
 };
