6215b713ee948b1fe1b4b0a1c85b8bdad92ebcc9
juandans01@gmail.com <juandans01@gmail.com>
2022-02-11 11:33:30 -0300

    test next.config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         source: process.env.NEXT_PUBLIC_API_URL + "/:path*",
         destination: process.env.NEXT_REDIRECT_API_URL + "/:path*",
         basePath: false,
-        locale: false,
+        // locale: false,
       },
     ];
   },
