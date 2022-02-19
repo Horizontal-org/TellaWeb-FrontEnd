e991af0a21d30dc5134850ab5ec97c1d951b5b85
Marcos Gutierrez <gmarcos87@gmail.com>
2022-02-19 12:26:09 +0100

    render only after router is ready, avoid rerenders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,17 @@ import type { AppProps } from "next/app";
 import { ToastWrapper } from "../components/ToastWrapper";
 import { Provider } from "react-redux";
 import store from "packages/state/store";
+import { useRouter } from "next/router";
 
 function TellaWeb({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <Provider store={store}>
       <ToastWrapper>
-        <Component {...pageProps} />
+        {router.isReady && <Component {...pageProps} />}
       </ToastWrapper>
     </Provider>
   );
 }
+
 export default appWithTranslation(TellaWeb);
