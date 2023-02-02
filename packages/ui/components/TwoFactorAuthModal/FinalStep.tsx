1cfe0b3437214986f4f1d66a1dac75a90ad6bb72
Gustavo <valbuena52@gmail.com>
2023-02-02 14:16:21 -0300

    add otp input

--- packages/ui/components/TwoFactorAuthModal/FinalStep.tsx
+++ packages/ui/components/TwoFactorAuthModal/FinalStep.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 import { Button } from '../..'
+import OtpInput from '../OtpInput.tsx/OtpInput'
 
 type Props = {
   toggle: () => void 
@@ -8,16 +9,18 @@ type Props = {
 export const FinalStep: FunctionComponent<React.PropsWithChildren<Props>> = ({ 
   toggle
 }) => {
-
+  const [otp, handleOtp] = useState<string>('')
   return (
     <>
       <p className='py-2 font-sans text-gray-600 text-xxxl font-bold'>
         Final step
       </p>
-      <p className='font-sans text-sm font-normal text-gray-500'>
+      <p className='font-sans text-base font-normal text-gray-500'>
         Enter the 6-digit code from your authentication app        
       </p>
-
+      <div className='flex justify-center py-4'>
+        <OtpInput value={otp} valueLength={6} onChange={(value) => handleOtp(value)}/>
+      </div>
       <div className='pb-4'>
         <Button 
           text='VERIFY'
