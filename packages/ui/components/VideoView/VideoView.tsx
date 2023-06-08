aa38979e3a55f0575b76c739abbc855a7d0a64c0
Gustavo <valbuena52@gmail.com>
2023-06-08 12:45:25 -0300

    remove log

--- packages/ui/components/VideoView/VideoView.tsx
+++ packages/ui/components/VideoView/VideoView.tsx
@@ -10,7 +10,6 @@ type Props = {
 };
 
 export const VideoView: FunctionComponent<React.PropsWithChildren<Props>> = ({ file }) => {
-  console.log(file)
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const {
     currentTime,
